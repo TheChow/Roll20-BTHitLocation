5a949a9cb1bf131fa4eca0d4d619e490276fd049
Chia-Hao Chang <chaihaoc@gmail.com>
2015-01-20 21:18:58 -0500

    changed to revealing module pattern

--- BTHitLocation.js
+++ BTHitLocation.js
@@ -1,13 +1,12 @@
-var BTHitLocation = {
-
-    ShootFront: function ShootFront(numShots) {
-        var hitLocs = [];
+var BTHitLocation = BTHitLocation || (function() {
+    function shootFront(numShots) {
+        var hitLocs = [];        
         for (i = 0; i < numShots; i++) {
             var roll = randomInteger(6) + randomInteger(6);
             var loc;
             switch (roll) {
                 case 2:
-                    loc = "Floating Crit";
+                    loc = "FloatingCrit";
                     break;
                 case 3:
                 case 4:
@@ -40,96 +39,101 @@ var BTHitLocation = {
             }
             hitLocs.push(loc);
         }
-        return hitLocs.sort();
-    },
-
-    ShootLeft: function ShootLeft(numShots) {
-        var hitLocs = [];
-        for (i = 0; i < numShots; i++) {
-            var roll = randomInteger(6) + randomInteger(6);
-            var loc;
-            switch (roll) {
-                case 2:
-                    loc = "Floating Crit";
-                    break;
-                case 3:
-                case 6:
-                    loc = "LL";
-                    break;
-                case 4:
-                case 5:
-                    loc = "LA";
-                    break;
-                case 7:
-                    loc = "LT";
-                    break;
-                case 8:
-                    loc = "CT";
-                    break;
-                case 9:
-                    loc = "RT";
-                    break;
-                case 10:
-                    loc = "RA";
-                    break;
-                case 11:
-                    loc = "RL";
-                    break;
-                case 12:
-                    loc = "HD";
-                    break;
-                default:
-                    loc = "miss";
-            }
-            hitLocs.push(loc);
-        }
-        return hitLocs.sort();
-    },
-
-    ShootRight: function ShootRight(numShots) {
-        var hitLocs = [];
-        for (i = 0; i < numShots; i++) {
-            var roll = randomInteger(6) + randomInteger(6);
-            var loc;
-            switch (roll) {
-                case 2:
-                    loc = "Floating Crit";
-                    break;
-                case 3:
-                case 6:
-                    loc = "RL";
-                    break;
-                case 4:
-                case 5:
-                    loc = "RA";
-                    break;
-                case 7:
-                    loc = "RT";
-                    break;
-                case 8:
-                    loc = "RT";
-                    break;
-                case 9:
-                    loc = "LT";
-                    break;
-                case 10:
-                    loc = "LA";
-                    break;
-                case 11:
-                    loc = "LL";
-                    break;
-                case 12:
-                    loc = "HD";
-                    break;
-                default:
-                    loc = "miss";
-            }
-            hitLocs.push(loc);
-        }
-
         return hitLocs.sort();
     }
-};
+
+    function shootLeft(numShots) {
+        var hitLocs = [];
+        for (i = 0; i < numShots; i++) {
+            var roll = randomInteger(6) + randomInteger(6);
+            var loc;
+            switch (roll) {
+                case 2:
+                    loc = "FloatingCrit";
+                    break;
+                case 3:
+                case 6:
+                    loc = "LL";
+                    break;
+                case 4:
+                case 5:
+                    loc = "LA";
+                    break;
+                case 7:
+                    loc = "LT";
+                    break;
+                case 8:
+                    loc = "CT";
+                    break;
+                case 9:
+                    loc = "RT";
+                    break;
+                case 10:
+                    loc = "RA";
+                    break;
+                case 11:
+                    loc = "RL";
+                    break;
+                case 12:
+                    loc = "HD";
+                    break;
+                default:
+                    loc = "miss";
+            }
+            hitLocs.push(loc);
+        }
+        return hitLocs.sort();
+    }
+
+    function shootRight(numShots) {
+        var hitLocs = [];
+        for (i = 0; i < numShots; i++) {
+            var roll = randomInteger(6) + randomInteger(6);
+            var loc;
+            switch (roll) {
+                case 2:
+                    loc = "FloatingCrit";
+                    break;
+                case 3:
+                case 6:
+                    loc = "RL";
+                    break;
+                case 4:
+                case 5:
+                    loc = "RA";
+                    break;
+                case 7:
+                    loc = "RT";
+                    break;
+                case 8:
+                    loc = "RT";
+                    break;
+                case 9:
+                    loc = "LT";
+                    break;
+                case 10:
+                    loc = "LA";
+                    break;
+                case 11:
+                    loc = "LL";
+                    break;
+                case 12:
+                    loc = "HD";
+                    break;
+                default:
+                    loc = "miss";
+            }
+            hitLocs.push(loc);
+        }
+        return hitLocs.sort();
+    }
+    
+    return {
+        ShootFront: shootFront,
+        ShootLeft: shootLeft,
+        ShootRight: shootRight
+    };
+})();
 
 on("chat:message", function (msg) {
     if (msg.type == "api" && msg.content.indexOf("!Front") !== -1) {
